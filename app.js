2bd563d2f12025a2ae7e16a47361d274e7cd38c0
DecLee <derricklee6@gmail.com>
2020-01-17 14:02:43 -0800

    added files and changes required for deployment to Heroku

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var app = express();
 app.use(helmet());
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://MangoUser:Mango@cluster0-wqsts.mongodb.net/local_library?retryWrites=true&w=majority';
+// Set up mongoose connection
+var dev_db_url = 'mongodb+srv://MangoUser:Mango@cluster0-wqsts.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
